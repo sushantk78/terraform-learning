46fc3f7aa383dc4e3698661e2a6203ed242d96d4
Sushant Chandra Kalia <sushantchandrak@gmail.com>
2024-09-24 14:41:59 +0530

    Deletion

--- terraform-resources/meta-argument-depends_on/terraform-manifests/c3-ec2-instance.tf
+++
@@ -1,9 +0,0 @@
-# Resource-8: Create EC2 Instance
-resource "aws_instance" "my-ec2-vm" {
-  ami                    = "ami-037774efca2da0726"
-  instance_type          = "t2.micro"
-  key_name               = "DemoKeyPair"
-  subnet_id              = aws_subnet.vpc-dev-public-subnet-1.id
-  vpc_security_group_ids = [aws_security_group.dev-vpc-sg.id]
-  user_data              = file("apache-install.sh")
-}
--- terraform-resources/meta-argument-for_each/v2-for_each-to-set/c2-iamuser.tf
+++
@@ -1,5 +0,0 @@
-# Create 4 IAM Users
-resource "aws_iam_user" "myuser" {
-  for_each = toset(["TJack", "TJames", "TMadhu", "TDave"])
-  name     = each.key
-}
