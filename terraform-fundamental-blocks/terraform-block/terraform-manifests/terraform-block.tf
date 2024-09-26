bba4c37c820e9f9601494c0fe3389ce52bfaa236
Sushant Chandra Kalia <sushantchandrak@gmail.com>
2024-09-26 13:47:19 +0530

    Adding new directories

---
+++ terraform-fundamental-blocks/terraform-block/terraform-manifests/terraform-block.tf
@@ -0,0 +1,14 @@
+# Terraform Block
+terraform {
+  required_version = "~> 1.9"
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.0"
+    }
+    random = {
+        source = "hashicorp/random"
+        version = "3.6.3"
+    }
+  }
+}
---
+++ terraform-fundamental-blocks/terraform-resources/resource-syntax-and-behaviour/terraform-manifests/c2-ec2-instance.tf
@@ -0,0 +1,12 @@
+# Create EC2 Instance
+
+resource "aws_instance" "my-ec2-vm" {
+  ami               = "ami-037774efca2da0726"
+  instance_type     = "t2.micro"
+  #availability_zone = "us-east-2a"
+  availability_zone = "us-east-2b"
+  tags = {
+    "Name" = "web"
+    "tag1" = "Update-test-1"
+  }
+}
