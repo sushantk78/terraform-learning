c62d1913880cf65d837d469d21ac929b685138f7
Sushant Chandra Kalia <sushantchandrak@gmail.com>
2024-09-28 15:46:53 +0530

    Deletion

--- terraform-fundamental-blocks/multiple-provider-configurations/terraform-manifests/c2-vpc-us-east-2.tf
+++
@@ -1,7 +0,0 @@
-# Resource Block to create VPC in us-east-2
-resource "aws_vpc" "vpc-us-east-2" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    "Name" = "vpc-us-east-2"
-  }
-}
--- terraform-fundamental-blocks/multiple-provider-configurations/terraform-manifests/c3-vpc-us-west-2.tf
+++
@@ -1,8 +0,0 @@
-# Resource Block to create VPC in us-west-2
-resource "aws_vpc" "vpc-us-west-2" {
-  cidr_block = "10.0.0.0/16"
-  provider = aws.sushant-us-west
-  tags = {
-    "Name" = "vpc-us-west-2"
-  }
-}
